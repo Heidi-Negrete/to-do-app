93ceb763a33c59732aad592bba887691e60d57cf
Heidi Negrete <heidi@negrete.dev>
2024-05-02 18:01:16 -0400

    add gitattributes file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import Todos, {type ToDo} from "./todos.ts";
 
+// Setup ToDoList
 const ToDoList = new Todos();
-
 // Example Todos
 ToDoList.add("Polish teeth", "personal");
 ToDoList.add("Pet Cactus", "work");
 
-console.log(ToDoList.getAll());
-console.log(ToDoList.getComplete());
+const list = document.querySelector("#todos-list") as HTMLUListElement;
+const form = document.querySelector("#todo-form") as HTMLFormElement;
